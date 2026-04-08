03aad3a15133771cb980eb39d320e0b6fe4a7dd4
santhugit-gmr <santhosh.thirumuru1916@gmail.com>
2026-04-08 15:18:21 +0530

    As part fo bug fix

--- main.tf
+++ main.tf
@@ -4,8 +4,10 @@ provider "aws" {
 
 resource "aws_instance" "devops_server" {
   ami           = "ami-01b14b7ad41e17ba4" # Amazon Linux
-  instance_type = "t2.micro"
+  instance_type = "t3.micro"
   key_name      = "nginx"
+  subnet_id = "subnet-0822b76743060b731"
+  associate_public_ip_address = true
 
   tags = {
     Name = "DevOps-Server"
