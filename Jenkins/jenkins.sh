a31b80a26b34deea3128c5b3c38d6e502ea1f469
santhugit-gmr <santhosh.thirumuru1916@gmail.com>
2026-04-08 16:22:17 +0530

    As part fo Bug fix

---
+++ Jenkins/jenkins.sh
@@ -0,0 +1,11 @@
+sudo yum upgrade
+sudo yum install java-21-amazon-corretto -y
+java -version
+sudo wget -O /etc/yum.repos.d/jenkins.repo \
+    https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+
+sudo yum install jenkins -y
+sudo systemctl enable jenkins
+sudo systemctl start jenkins
+sudo systemctl status jenkins
--- main.tf
+++ main.tf
@@ -13,3 +13,7 @@ resource "aws_instance" "devops_server" {
     Name = "DevOps-Server"
   }
 }
+
+output "ec2_public_ip" {
+  value = aws_instance.devops_server.public_ip
+}
