c3a0f7d0287da8278e4727f0f0152d098bc27abf
santhugit-gmr <santhosh.thirumuru1916@gmail.com>
2026-04-09 11:41:19 +0530

    As part of fix

---
+++ simple-devops-project/Terraform/main.tf
@@ -0,0 +1,19 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "devops_server" {
+  ami           = "ami-01b14b7ad41e17ba4" # Amazon Linux
+  instance_type = "t3.micro"
+  key_name      = "nginx"
+  subnet_id = "subnet-0822b76743060b731"
+  associate_public_ip_address = true
+
+  tags = {
+    Name = "DevOps-Server"
+  }
+}
+
+output "ec2_public_ip" {
+  value = aws_instance.devops_server.public_ip
+}
